be8045a68a2db64e9ad3a476f8e2481e0a39dbeb
unknown <games.dynamy@gmail.com>
2024-10-30 16:09:15 +0200

    Andmetöötlus

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 #from moodle.core.webservice import BaseWebservice
 from os import environ
 from requests import post
+from datetime import datetime, timedelta
 
 class MoodleAPI:
     def __init__(self, url, token):
         self.url = url
         self.token = token
+        self.kursuste_info = self.hangi_kursused()
+        self.kalendri_info = self.hangi_kalendri_info([7550, 1755, 12774, 10892, 500, 8434])
     
     # Kutsub Moodle'i API-d kindla argumendiga vastavalt küsitavale infole
-    def call(self, funktsioon, data):
+    def call(self, funktsioon:str, data:dict={}) -> None:
         argumendid = {
             "wstoken": self.token,
             "wsfunction": funktsioon,
@@ -24,33 +27,48 @@ class MoodleAPI:
 
         return vastus2
 
-    # Leiab ja prindib kõik viimatised kursused Moodle'is
-    def hangi_kursused(self):
-        kursuste_list = self.call("core_course_get_recent_courses", {})
+    # Leiab ja tagastab kõigi viimatiste Moodle'i kursuste info dictina
+    def hangi_kursused(self) -> dict:
+        kursuste_list = self.call("core_course_get_recent_courses")
+        kursuste_dict = {}
+        kursuste_nimed_list = []
+        kursuste_id_list = []
+        kursuste_kood_list = []
         for kursus in kursuste_list:
+            kood = kursus["shortname"]
             id = kursus["id"]
             nimi = kursus["fullname"]
-            kood = kursus["shortname"]
-            print(nimi, id)
+            kursuste_dict[kood] = [id, nimi]
+            kursuste_id_list.append(id)
+            kursuste_nimed_list.append(nimi)
+            kursuste_kood_list.append(kood)
+        return kursuste_dict
 
     # Leiab ja prindib kõik Moodle'i kalendri sündmused
-    def hangi_kalendri_info(self):
+    def hangi_kalendri_info(self, kursuste_idd:list) -> dict:
+        unix_hetkeaeg = int((datetime.now() - datetime(1970, 1, 1)).total_seconds())
+        unixaeg_3_ndl_parast = int((datetime.now() + timedelta(weeks=3) - datetime(1970, 1, 1)).total_seconds())
         data = {
             "options[userevents]": 1,
             "options[siteevents]": 1,
-            "options[timestart]": 1730255400,
-            "options[timeend]": 1732069800,
-            "events[courseids][0]": 7550,
-            "events[courseids][1]": 1755,
-            "events[courseids][2]": 12774,
-            "events[courseids][3]": 10892,
-            "events[courseids][4]": 500,
-            "events[courseids][5]": 8434
+            "options[timestart]": unix_hetkeaeg,
+            "options[timeend]": unixaeg_3_ndl_parast,
         }
+        for idx, kursuse_id in enumerate(kursuste_idd):
+            data[f"events[courseids][{idx}]"] = kursuse_id
         moodle_vastus = self.call("core_calendar_get_calendar_events", data)
         sundmused = moodle_vastus["events"]
+        sundmuste_list = []
         for sundmus in sundmused:
-            print(sundmus["name"])
+            pealkiri = sundmus["name"]
+            kirjeldus = sundmus["description"].strip()
+            sundmuse_kursuse_id = sundmus["courseid"]
+            ulesande_tuup = sundmus["modulename"]
+            ulesande_algusaeg = sundmus["timestart"]
+            sundmuste_list.append([pealkiri, sundmuse_kursuse_id, ulesande_tuup, ulesande_algusaeg])
+        return sundmuste_list
+
+        
 
 
 
@@ -64,8 +82,9 @@ def main():
 
     # Proovin kutsuda call funktsiooni, et saada mingigi vastus Moodlelt
     try:
-        moodle_api.hangi_kalendri_info()
-        #moodle_api.hangi_kursused()
+        info = moodle_api.kalendri_info
+        for sundmus in info:
+            print(sundmus[0], datetime.fromtimestamp(sundmus[-1]).strftime('%Y-%m-%d %H:%M'))
     except Exception as viga:
         print(f"Error: {viga}")
 
